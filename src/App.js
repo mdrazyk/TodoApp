c82af1655d3fd8ab661b7da451e0e0255340d511
Mateusz Drażyk <drazyk.mateusz@gmail.com>
2017-11-05 00:00:32 +0100

    todos styling

--- src/App.js
+++ src/App.js
@@ -16,24 +16,59 @@ class App extends Component {
         </nav>
         <div className="App-body">
           <div className="Todo">
-            <p>Kupić masło</p>
+            <p className="Todo-header">Kupić masło</p>
             <p>
               Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum
               Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum
               Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum
               Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum
             </p>
-            <div>
-              <p style ={{fontSize: "30px"}}>
-                &#10006;
+            <div className="Todo-footer">
+              <p style ={{fontSize: "40px", color: "#FF6F00"}}>
+                &#10005;
               </p>
-              <p style ={{fontSize: "30px"}}>
-                &#10004;
+              <p  style ={{fontSize: "40px"}}>
+                &#8801;
+              </p>
+              <p style ={{fontSize: "40px", color: "#4CAF50"}}>
+                &#10003;
               </p>
             </div>
           </div>
-          <div>asdasd</div>
-          <div>asdasd</div>
+          <div>
+           <div className="Todo">
+            <p className="Todo-header">Wyciągnąć ze zmywarki</p>
+            <p>
+              Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum
+              Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum
+
+            </p>
+                <div className="Todo-footer">
+                <p style ={{fontSize: "40px", color: "#FF6F00"}}>
+                  &#10005;
+                </p>
+                <p  style ={{fontSize: "40px"}}>
+                  &#8801;
+                </p>
+                <p style ={{fontSize: "40px", color: "#4CAF50"}}>
+                  &#10003;
+                </p>
+              </div>
+            </div>
+          </div>
+          <div>
+          <div className="Todo-single">
+            <p className="Todo-header">Wyciągnąć ze zmywarki</p>
+                <div className="Todo-footer-single">
+                <p style ={{fontSize: "40px", color: "#FF6F00"}}>
+                  &#10005;
+                </p>
+                <p style ={{fontSize: "40px", color: "#4CAF50"}}>
+                 &#10003;
+                </p>
+              </div>
+            </div>
+          </div>
           <div>asdasd</div>
           <div>asdasd</div>
           <div>asdasd</div>
